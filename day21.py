22e6bfa15bb2f93230182ab7f72bc3ee03d341d2
Stav <stav1472@gmail.com>
2023-12-02 19:22:42 +0200

    adding all days

---
+++ day21.py
@@ -0,0 +1,20 @@
+from aocd import get_data
+
+
+def part1(lines):
+    return lines
+
+
+def part2(lines):
+    return lines
+
+
+def main():
+    lines = get_data(day=21, year=2023).splitlines()
+    print('day 21')
+    print(f'part1: {part1(lines)}')
+    print(f'part2: {part2(lines)}')
+
+
+if __name__ == '__main__':
+    main()
