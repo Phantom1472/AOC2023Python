c8e43f0ada92c73b08d94da5aa679f528c3cc485
Stav <stav1472@gmail.com>
2023-12-05 23:12:12 +0200

    day 4 parts 1&2 finished

--- day04.py
+++ day04.py
@@ -1,12 +1,27 @@
+from collections import Counter
+
 from aocd import get_data
 
 
 def part1(lines):
-    return lines
+    total = 0
+    for line in lines:
+        card = list(map(str.split, line.split(':')[1].split('|')))
+        card = [set(map(int, x)) for x in card]
+        winning_numbers = len(card[0] & card[1])
+        if winning_numbers:
+            total += 2 ** (winning_numbers - 1)
+    return total
 
 
 def part2(lines):
-    return lines
+    card_copies = Counter(range(1, len(lines) + 1))
+    for index, line in enumerate(lines, 1):
+        card = list(map(str.split, line.split(':')[1].split('|')))
+        card = [set(map(int, x)) for x in card]
+        for num in range(index + 1, index + 1 + len(card[0] & card[1])):
+            card_copies[num] += card_copies[index]
+    return card_copies.total()
 
 
 def main():
