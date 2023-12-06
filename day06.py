e637a56ce002037b9e7bc9ce491c97ec17f29474
Stav <stav1472@gmail.com>
2023-12-06 07:33:45 +0200

    day 6 parts 1&2 finished

--- day06.py
+++ day06.py
@@ -1,12 +1,20 @@
+import math
+
 from aocd import get_data
 
 
 def part1(lines):
-    return lines
+    times, distances = [list(map(int, line.split(':')[1].split())) for line in lines]
+    ways_list = []
+    for time, distance in zip(times, distances):
+        ways_list.append(
+            len(list(filter(lambda x: x > distance, map(math.prod, zip(range(0, time), range(time, 0, -1)))))))
+    return math.prod(ways_list)
 
 
 def part2(lines):
-    return lines
+    time, distance = [int(line.replace(' ', '').split(':')[1]) for line in lines]
+    return len(list(filter(lambda x: x > distance, map(math.prod, zip(range(0, time), range(time, 0, -1))))))
 
 
 def main():
