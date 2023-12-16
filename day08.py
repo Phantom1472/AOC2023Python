aecb3c8b37191ed1b6f76fabbf2593f67300baa2
Stav <stav1472@gmail.com>
2023-12-16 11:54:20 +0200

    day 8 parts 1&2 finished

--- day08.py
+++ day08.py
@@ -1,12 +1,47 @@
+import math
+import re
+from itertools import cycle
+
 from aocd import get_data
 
 
 def part1(lines):
-    return lines
+    instructions = lines[0]
+    raw_network = lines[2:]
+    network = {}
+    for node in raw_network:
+        start, left, right = re.split(r'\W+', node)[:-1]
+        network[start] = {'L': left, 'R': right}
+    current = 'AAA'
+    steps = 0
+    for instruction in cycle(instructions):
+        next_node = network[current][instruction]
+        steps += 1
+        if next_node == 'ZZZ':
+            break
+        current = next_node
+    return steps
 
 
 def part2(lines):
-    return lines
+    instructions = lines[0]
+    raw_network = lines[2:]
+    network = {}
+    for node in raw_network:
+        start, left, right = re.split(r'\W+', node)[:-1]
+        network[start] = {'L': left, 'R': right}
+    currents = [node for node in network if node[-1] == 'A']
+    lengths = []
+    for current in currents:
+        steps = 0
+        for instruction in cycle(instructions):
+            next_node = network[current][instruction]
+            steps += 1
+            if next_node[-1] == 'Z':
+                lengths.append(steps)
+                break
+            current = next_node
+    return math.lcm(*lengths)
 
 
 def main():
