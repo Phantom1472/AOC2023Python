b446fc9fda503db365ddc665626a24683d0b1605
Stav <stav1472@gmail.com>
2023-12-17 05:15:56 +0200

    day 9 parts 1&2 finished

--- day09.py
+++ day09.py
@@ -2,11 +2,28 @@ from aocd import get_data
 
 
 def part1(lines):
-    return lines
+    return aux(lines)
 
 
 def part2(lines):
-    return lines
+    return aux(lines, -1)
+
+
+def aux(lines, direction=1):
+    lines = [[int(x) for x in line.split()][::direction] for line in lines]
+    total = 0
+    for line in lines:
+        new_lines = [line]
+        while True:
+            if len(set(new_lines[-1])) == 1:
+                break
+            else:
+                last_line = new_lines[-1]
+                z = zip(last_line, last_line[1:])
+                new_last_line = [y - x for x, y in z]
+                new_lines.append(new_last_line)
+        total += sum([line[-1] for line in new_lines])
+    return total
 
 
 def main():
